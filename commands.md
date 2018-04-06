f20de9e2a476cbe9b13243f3a50ad79e599f9d1b
Xiao Ling <lingxiao@seas.upenn.edu>
2018-04-06 15:48:23 -0400

    first commit

---
+++ commands.md
@@ -0,0 +1,12 @@
+
+## configure system for tensorflow ##
+
+0. pip uninstall virtualenv
+1. sudo conda update --all
+2. conda install virtualenv
+3. cd ~/
+4. mkdir tensorflow-master
+5. cd tensorflow-master
+6. virtualenv --system-site-packages .
+
+## install tensorflow ## 
---
+++ project-1/main.py
@@ -0,0 +1,16 @@
+############################################################
+# Module  : project 1: main
+# Date    : April 2nd, 2018
+# Author  : xiao ling
+############################################################
+
+from __future__ import print_function
+
+import os
+import nltk
+import pickle
+import numpy as np
+
+
+
+print ("hello world")
